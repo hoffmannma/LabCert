48bceee3532175549f4b3ac99c96eb793caf2258
Edu <eharwart@gmail.com>
2016-05-16 10:57:25 +0200

    Initial Commit

---
+++ app/src/main/java/de/th_nuernberg/harwedu/labcert/CsvParser.java
@@ -0,0 +1,50 @@
+package de.th_nuernberg.harwedu.labcert;
+
+/**
+ * Created by Edu on 15.05.2016.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.content.Context;
+
+public class CsvParser {
+
+    Context context;
+    String file_name;
+    ArrayList<HashMap<String, String>> CSVData;
+
+    public CsvParser(Context context, String file_name) {
+        this.context = context;
+        this.file_name = file_name;
+    }
+
+    public ArrayList<HashMap<String, String>> ReadCSV() throws IOException {
+
+        InputStream instream = context.getAssets().open(file_name);
+        InputStreamReader instreamreader = new InputStreamReader(instream);
+        BufferedReader br = new BufferedReader(instreamreader);
+        String line;
+        String csvSplit = ",";
+
+        //br.readLine();
+        CSVData = new ArrayList<>();
+
+        while ((line = br.readLine()) != null) {
+
+            String[] row = line.split(csvSplit);
+            HashMap<String, String> hm = new HashMap<>();
+
+            for (int i = 0; i < row.length; i++) {
+                hm.put("row[" + i + "]", row[i]);
+            }
+            CSVData.add(hm);
+        }
+        return CSVData;
+    }
+
+}
---
+++ app/src/main/java/de/th_nuernberg/harwedu/labcert/MainActivity.java
@@ -0,0 +1,155 @@
+package de.th_nuernberg.harwedu.labcert;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private TextView scanFormatTxt;
+    private TextView scanContentTxt;
+
+    // Initialize
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //scanFormatTxt = (TextView)findViewById(R.id.scan_format_TV);
+        //scanContentTxt = (TextView)findViewById(R.id.scan_content_TV);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*assert fab != null;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "<Call Scanner>", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                IntentIntegrator scanIntegrator = new IntentIntegrator();
+                scanIntegrator.initiateScan();
+            }
+        });*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        assert drawer != null;
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            Fragment f = new GroupDataFragment();
+            ft.replace(R.id.content_frame, f);
+        }
+    }
+
+    public void fabClicked(View v){
+        IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+        scanIntegrator.initiateScan();
+    }
+
+    // show format and content
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        IntentResult scanningResult =
+                IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String scanFormat = scanningResult.getFormatName();
+            String scanContent = scanningResult.getContents();
+           // scanFormatTxt.setText(scanFormat);
+           // scanContentTxt.setText(scanContent);
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    // Called when (hardware) back key pressed
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    // Create Options Menu (upper action bar)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_manage_group) {
+            // TODO: Code für Fragmentinput überarbeiten
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            Fragment f = new GroupDataFragment();
+            ft.replace(R.id.content_frame, f);
+        }
+        else if (id == R.id.nav_switch_group) {
+
+        }
+        else if (id == R.id.nav_new_group) {
+
+        }
+        else if (id == R.id.nav_export_csv) {
+
+        }
+        else if (id == R.id.nav_accompl) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
